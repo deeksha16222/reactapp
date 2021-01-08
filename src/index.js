c9d848072c78df1e77c240d11cabc53ff670addd
deeksha16222 <56512924+deeksha16222@users.noreply.github.com>
2021-01-08 13:15:20 +0530

    adding for first time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<>
+<Card imgsrc = "https://im.indiatimes.in/content/2020/Aug/stranger-things-1_5f2badcd2f253.jpg?w=725&h=407"
+ title = "Best Netflix series tocmd watch"
+sname = "Stranger Things"
+link = "https://www.youtube.com/watch?v=mnd7sFt5c3A"
+/>
+<Card imgsrc = "https://tse3.mm.bing.net/th?id=OIP.rvUcdjze3CaZ8xU2iQ-N-AHaHa&pid=Api&P=0&w=300&h=300"
+title = "Best Netflix series to watch"
+sname = "The Queen's Gambit"
+link= "https://www.youtube.com/watch?v=CDrieqwSdgI"
+/>
+<Card imgsrc = "https://magazine99.com/wp-content/uploads/2020/10/4229968.jpg"
+title = "Best Netflix series to watch"
+sname = "Emily in Paris"
+link= "https://www.youtube.com/watch?v=lptctjAT-Mk"
+/>
+<Card imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAs6c0zZR8ZChbWMq6-H92nf3D_YT_rEPqPw&usqp=CAU"
+title = "Best Netflix series to watch"
+sname = "Lucifer"
+link = "https://www.youtube.com/watch?v=X4bF_quwNtw"
+/>
+</>,
+document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
