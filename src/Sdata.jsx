bdd7ba1c486d2b834bd49bbbe5962c2609cb4954
deeksha16222 <56512924+deeksha16222@users.noreply.github.com>
2021-01-08 14:35:23 +0530

    second time commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,31 @@
+const Sdata = [
+{
+sname : "Stranger Things",
+imgsrc : "https://im.indiatimes.in/content/2020/Aug/stranger-things-1_5f2badcd2f253.jpg?w=725&h=407",
+title : "Best Netflix series to watch",
+link : "https://www.youtube.com/watch?v=mnd7sFt5c3A",
+},
+
+{
+sname : "The Queen's Gambit",
+imgsrc : "https://tse3.mm.bing.net/th?id=OIP.rvUcdjze3CaZ8xU2iQ-N-AHaHa&pid=Api&P=0&w=300&h=300",
+title :"Best Netflix series to watch",
+link : "https://www.youtube.com/watch?v=CDrieqwSdgI",
+},
+
+{
+sname : "The Queen's Gambit",
+imgsrc : "https://magazine99.com/wp-content/uploads/2020/10/4229968.jpg",
+title : "Best Netflix series to watch",
+link : "https://www.youtube.com/watch?v=lptctjAT-Mk",
+},
+
+{
+sname : "Lucifer",  
+imgsrc : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAs6c0zZR8ZChbWMq6-H92nf3D_YT_rEPqPw&usqp=CAU",
+title : "Best Netflix series to watch",
+link : "https://www.youtube.com/watch?v=X4bF_quwNtw",
+},
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Cards";
+import Sdata from "./Sdata";
 
 
 ReactDOM.render(
 <>
-<Card imgsrc = "https://im.indiatimes.in/content/2020/Aug/stranger-things-1_5f2badcd2f253.jpg?w=725&h=407"
- title = "Best Netflix series tocmd watch"
-sname = "Stranger Things"
-link = "https://www.youtube.com/watch?v=mnd7sFt5c3A"
+<h1 className="heading_style"> LIST OF TOP 5 NETFLIX SERIES IN 2020 </h1>
+
+<Card sname = {Sdata[0].sname}
+imgsrc = {Sdata[0].imgsrc}
+title = {Sdata[0].title}
+link = {Sdata[0].link}
 />
-<Card imgsrc = "https://tse3.mm.bing.net/th?id=OIP.rvUcdjze3CaZ8xU2iQ-N-AHaHa&pid=Api&P=0&w=300&h=300"
-title = "Best Netflix series to watch"
-sname = "The Queen's Gambit"
-link= "https://www.youtube.com/watch?v=CDrieqwSdgI"
+<Card sname = {Sdata[1].sname}
+imgsrc = {Sdata[1].imgsrc}
+title = {Sdata[1].title}
+link = {Sdata[1].link}
 />
-<Card imgsrc = "https://magazine99.com/wp-content/uploads/2020/10/4229968.jpg"
-title = "Best Netflix series to watch"
-sname = "Emily in Paris"
-link= "https://www.youtube.com/watch?v=lptctjAT-Mk"
+<Card sname = {Sdata[2].sname}
+imgsrc = {Sdata[2].imgsrc}
+title = {Sdata[2].title}
+link = {Sdata[2].link}
 />
-<Card imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAs6c0zZR8ZChbWMq6-H92nf3D_YT_rEPqPw&usqp=CAU"
-title = "Best Netflix series to watch"
-sname = "Lucifer"
-link = "https://www.youtube.com/watch?v=X4bF_quwNtw"
+<Card sname = {Sdata[3].sname}
+imgsrc = {Sdata[3].imgsrc}
+title = {Sdata[3].title}
+link = {Sdata[3].link}
 />
 </>,
 document.getElementById("root")
